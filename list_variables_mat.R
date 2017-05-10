a46698bb0a196a36442b2a8325f9f24891936237
Mychau <legeaymylene@gmail.com>
2017-05-10 20:21:07 +0200

    Update list_variables_mat.R

--- list_variables_mat.R
+++ list_variables_mat.R
@@ -2,8 +2,7 @@
 
 #Inputs: Matrix or sparse matrix
 
-
-#Renvoie un objet list_variables:
+#Return object list_variables:
 
 #list_variables$class    (class of the input)
 #$variable1
@@ -15,70 +14,66 @@
 
 require(Matrix)
 
-
 list_variables_matrix=function(base){
   require(Matrix)
   if(class(base)=="dgCMatrix"){
     pattern=attr(base,"assign")
     variables=c()
     class=c()
-    #namefac=NA
     racines=c()
     list_variables=c()
     names_col=c()
     list_variables=c(class(base))
-    #print(list_variables)
+
     for(i in 1:(length(pattern)-1)){
-      if(length(pattern[which(pattern==pattern[i])])==1){
-        variables=c(variables,colnames(base)[i])#find all not factor variables
+      if(length(pattern[which(pattern==pattern[i])])==1){#for non factor variables
+        variables=c(variables,colnames(base)[i])#find non factor variables
         names_col=c(names_col,colnames(base)[i])
-        temp=list(Class="numeric",Levels=NULL)#ne pas rajouter si racine déjà dedans
+        temp=list(Class="numeric",Levels=NULL)
         all=list(variable=temp)
         list_variables=c(list_variables,all)}
       
-      else{#Obtenir la racine de la variable
-        pos=pattern[which(pattern==pattern[i])]
-        #print(pos)
+      else{#for factor variables
+        pos=pattern[which(pattern==pattern[i])]#find the columns corresponding to a variable
         allnames=colnames(base)[pos]
         all_val=c()
         list_char=c()
         
         for(value in allnames){
-          list_char=c(list_char,strsplit(value,""))
+          list_char=c(list_char,strsplit(value,""))#obtain a list of the letters of the column
         }
         size=c()
         for(value in list_char){
           y=length(unlist(value))
-          size=c(size,y)
+          size=c(size,y)#find length
         }
         stops=c()
         for(i in 1:(length(list_char)-1)){
           for(j in 1:min(size)){
-            if(unlist(list_char[i])[j]!=unlist(list_char[i+1])[j]){
+            if(unlist(list_char[i])[j]!=unlist(list_char[i+1])[j]){#find the first different letter
               stops=c(stops,j-1)
               break
             }
           }
         }
-        racine=paste(unlist(list_char[1])[1:min(stops)],sep="",collapse="")
-        if(!racine %in% racines){
+        racine=paste(unlist(list_char[1])[1:min(stops)],sep="",collapse="")#create the root (name of the variable)
+        if(!racine %in% racines){#check if is already in the list of variables, don't add it
           racines=c(racines,racine)
           names_col=c(names_col,racine)
           Levels=c()
           for(value in list_char){
-            niv=paste(unlist(value)[(min(stops)+1):length(value)],sep="",collapse="")
+            niv=paste(unlist(value)[(min(stops)+1):length(value)],sep="",collapse="")#find the level and add it to the levels of the variable
             Levels=c(Levels,niv)
           }
-          temp=list(Class="factor",Levels=Levels)#ne pas rajouter si racine déjà dedans
+          temp=list(Class="factor",Levels=Levels)
           all=list(variable=temp)
           list_variables=c(list_variables,all)
-          #all_match=c(all_match,temp)}
         }
       }
-      if(pattern[length(pattern)-1])==pattern[length(pattern)]){
+      if(pattern[length(pattern)-1])==pattern[length(pattern)]){#for the last column, if not a factor
         variables=c(variables,colnames(base)[i])
         names_col=c(names_col,colnames(base)[i])
-        temp=list(Class="numeric",Levels=NULL)#ne pas rajouter si racine déjà dedans
+        temp=list(Class="numeric",Levels=NULL)
         all=list(variable=temp)
         list_variables=c(list_variables,all)}
     }
@@ -88,7 +83,6 @@ list_variables_matrix=function(base){
   if(is.matrix(base)){
     variables=c()
     class=c()
-    #namefac=NA
     racines=c()
     list_variables=c()
     names_col=c()
@@ -104,10 +98,8 @@ list_variables_matrix=function(base){
       all=list(variable=temp)
       list_variables=c(list_variables,all)
     }
-    #print(names_col)
     names(list_variables)<-c("class",names_col)
   }
   setClass("list_variables",slots=c("list"))
-  #class(list_variables)="list_variables"
   return(list_variables)
 }
